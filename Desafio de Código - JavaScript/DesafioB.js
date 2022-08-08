6f24fde023b16ec0efcb10e622033b50f887b9f5
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-08-08 11:07:28 -0400

    Desafio B - Completo

--- Desafio de Código - JavaScript/DesafioB.js
+++ Desafio de Código - JavaScript/DesafioB.js
@@ -1,4 +1,4 @@
-// Obs: A função gets é implementada dentro do sistema para ler as entradas (inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Obs: A função gets é implementada dentro do sistema para ler as entradas (inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n").
 
 let line = gets().split(" ");
 let A = parseInt(line[0]);
